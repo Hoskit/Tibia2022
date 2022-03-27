4aec61e69311cbf663c834e3508d61f44fe27fee
tonyye333@gmail.com <tonyye333@gmail.com>
2022-03-27 14:40:13 +0200

    Rebalance knight healing spells

--- data/scripts/spells/healing/intense_recovery.lua
+++ data/scripts/spells/healing/intense_recovery.lua
@@ -1,12 +1,12 @@
 local combat = Combat()
-local cooldown = 60000 -- milis
+local cooldown = 300000 -- milis
 
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_GREEN)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 
 local condition = Condition(CONDITION_REGENERATION)
 condition:setParameter(CONDITION_PARAM_TICKS, cooldown)
-condition:setParameter(CONDITION_PARAM_HEALTHGAIN, 40)
+condition:setParameter(CONDITION_PARAM_HEALTHGAIN, 100)
 condition:setParameter(CONDITION_PARAM_HEALTHTICKS, 3000) -- 3sec
 condition:setParameter(CONDITION_PARAM_BUFF_SPELL, true)
 combat:addCondition(condition)
