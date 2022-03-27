1ed7977ffca24f362b065364db4f06d4bd4f9f1b
tonyye333@gmail.com <tonyye333@gmail.com>
2022-03-28 01:32:33 +0200

    Rebalance some knight healing spells

--- data/scripts/spells/healing/intense_wound_cleansing.lua
+++ data/scripts/spells/healing/intense_wound_cleansing.lua
@@ -5,8 +5,8 @@ combat:setParameter(COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 
 function onGetFormulaValues(player, level, magicLevel)
-	local min = (level + magicLevel * 70) + 438
-	local max = (level + magicLevel * 92) + 544
+	local min = ((level + magicLevel) * 15) + 438
+	local max = ((level + magicLevel) * 20) + 544
 	return min, max
 end
 
