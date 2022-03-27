3d0fca484e7ff5e0ab6970c50997ae169a698b14
tonyye333@gmail.com <tonyye333@gmail.com>
2022-03-27 16:04:32 +0200

    Make GFB viable for knights

--- data/scripts/spells/runes/great_fireball.lua
+++ data/scripts/spells/runes/great_fireball.lua
@@ -5,8 +5,8 @@ combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_FIRE)
 combat:setArea(createCombatArea(AREA_CIRCLE3X3))
 
 function onGetFormulaValues(player, level, maglevel)
-	local min = (level / 5) + (maglevel * 1.2) + 7
-	local max = (level / 5) + (maglevel * 2.8) + 17
+	local min = (level) + (maglevel * 1.2) + 47
+	local max = (level) + (maglevel * 2.8) + 77
 	return -min, -max
 end
 combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
@@ -23,7 +23,7 @@ rune:name("great fireball rune")
 rune:runeId(3191)
 rune:allowFarUse(true)
 rune:charges(4)
-rune:level(30)
+rune:level(14)
 rune:magicLevel(4)
 rune:cooldown(2 * 1000)
 rune:groupCooldown(2 * 1000)
