aea34c86763464dd199b0bcb33d8c888d5fde326
tonyye333@gmail.com <tonyye333@gmail.com>
2022-04-02 19:16:12 +0200

    Nerf great fireball rune more

--- data/scripts/spells/conjuring/great_fireball_rune.lua
+++ data/scripts/spells/conjuring/great_fireball_rune.lua
@@ -1,7 +1,7 @@
 local spell = Spell("instant")
 
 function spell.onCastSpell(creature, variant)
-	return creature:conjureItem(3147, 3191, 4)
+	return creature:conjureItem(3147, 3191, 3)
 end
 
 spell:name("Great Fireball Rune")
@@ -11,7 +11,7 @@ spell:vocation("sorcerer;true", "master sorcerer;true", "knight;true", "elite kn
 spell:cooldown(2 * 1000)
 spell:groupCooldown(2 * 1000)
 spell:level(12)
-spell:mana(230)
+spell:mana(530)
 spell:soul(3)
 spell:isAggressive(false)
 spell:needLearn(false)
